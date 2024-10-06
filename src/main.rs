88639a5b9a119b0aa0d04b78c9a7322a5d4fce9f
Dan Plubell <danplubell@gmail.com>
2024-10-06 10:05:57 -0500

    Add initial screen

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 use bracket_lib::prelude::*;
 struct State {}
 
-fn main() {
-    println!("Hello, world!");
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello, Bracket Terminal");
+    }
+}
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("Flappy Bird")
+        .build()?;
+    main_loop(context, State{})
+    
 }
