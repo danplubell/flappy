a90ce3f64a07dc4b9f0729000756be93dba099d4
Dan Plubell <danplubell@gmail.com>
2024-10-06 10:11:52 -0500

    Add game mode enum and add mode field to State

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
 use bracket_lib::prelude::*;
-struct State {}
-
+struct State {
+    mode: GameMode
+}
+impl State {
+    fn new()-> Self {
+        Self {
+            mode: GameMode::Menu,
+        }
+    }
+}
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
         ctx.print(1, 1, "Hello, Bracket Terminal");
     }
+}
+enum GameMode {
+    Menu,
+    Playing,
+    End,
 }
 fn main() -> BError {
     let context = BTermBuilder::simple80x50()
